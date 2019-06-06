d40a85f2ff680e9fb07edc5aa4795c76a4d4d023
Daniel <ricardodanmonti52468@hotmail.com>
2019-06-05 21:15:52 -0500

    cambios richi

--- app/src/main/java/com/example/modelosdered/Main2ActivityIP.java
+++ app/src/main/java/com/example/modelosdered/Main2ActivityIP.java
@@ -14,19 +14,21 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class Main2ActivityIP extends AppCompatActivity implements View.OnClickListener {
-    EditText a,b,c,d,div,noRedes;
-    TextView ver,mr1,di1,df1,result;
+    EditText a,b,c,d,div,noRedes,q;
+    TextView ver,mr1,di1,df1,result,result2;
     Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2_ip);
+        setContentView(R.layout.activity);
 
 
         a = (EditText) findViewById(R.id.o1);
         result = (TextView) findViewById(R.id.TextViewRes);
+        result2 = (TextView) findViewById(R.id.hola);
         b = (EditText) findViewById(R.id.o2);
         c = (EditText)findViewById(R.id.o3);
+        q = (EditText) findViewById(R.id.o4);
         d = (EditText) findViewById(R.id.o4);
         b1 = (Button) findViewById(R.id.b1);
         div = (EditText) findViewById(R.id.sub);
@@ -34,186 +36,223 @@ public class Main2ActivityIP extends AppCompatActivity implements View.OnClickLi
         mr1 = (TextView) findViewById(R.id.mr);
         di1 = (TextView) findViewById(R.id.di);
         df1 = (TextView) findViewById(R.id.df);
+
         b1.setOnClickListener(this);
 
     }
 
-    public void noRedes(){//aqui es donde esta el calculo de N y lo que falta
-        noRedes=(EditText) findViewById(R.id.noRedes);
-        int N;
-        int noR= Integer.parseInt(noRedes.getText().toString());
-        double n=(Math.log(noR))/(Math.log(2));
-        int be=(int)Math.ceil(n);
-        System.out.println(be);//aqui funciona
-        //********
-        int a1 = Integer.parseInt(a.getText().toString());
-        int b1 = Integer.parseInt(b.getText().toString());
-        int c1 = Integer.parseInt(c.getText().toString());
-        int d1 = Integer.parseInt(d.getText().toString());
-        int mas= Integer.parseInt(div.getText().toString());
-        int e1 =mas+be;
-        if (e1<=30 ) {
-            ver.setText("Direccion y division correcta");
 
-            //probando
-            if (e1 < 8) {
-                int resta = 8 - e1;//saber la posicion de la ultima x en el octeto
-                int salto = (int) Math.pow(2, resta);//proximo salto para referencia
-                int fin = a1 + salto - 1;
-                mr1.setText(a1 + ".0.0.0");
-                di1.setText(a1 + ".0.0.1");
-                if (fin > 255) {//si pasa de 255
-                    int sob = fin - 255 - 1;
-                    df1.setText((a1 + 1) + "." + sob + ".255.254/" + e1);
-                } else
-                    df1.setText(fin + ".255.255.254/" + e1);
-            }
-
-            else if (e1 > 8 && e1 <= 16) {//segundo octeto direccion ip
-                int resta = 16 - e1;
-                int salto = (int) Math.pow(2, resta);
-                int fin = b1 + salto - 1;
-                mr1.setText(a1 + "." + b1 + ".0.0/" + e1);
-                di1.setText(a1 + "." + b1 + ".0.1/" + e1);
-                if (fin > 255) {//si pasa de 255
-                    int sob = fin -255- 1;
-                    df1.setText((a1 + 1) + "." + sob + ".255.254/" + e1);
-                }
-                else
-                    df1.setText(a1 + "." + fin + ".255.254/" + e1);
-            }
-            else if (e1>16 && e1<=24 ){//tercer octeto direccion ip
-                int resta = 24 - e1;
-                int salto = (int) Math.pow(2, resta);//salto
-
-                for (int i=0;i<=be;i++){
-                    int fin = c1+salto-1;//final
-                    int fin2=b1+1;
-                    mr1.setText(a1+"."+b1+"."+c1+".0/"+e1);
-                    di1.setText(a1+"."+b1+"."+c1+".1/"+e1);
-
-                    System.out.println(a1+"."+b1+"."+c1+".0/"+e1);
-                    System.out.println(a1+"."+b1+"."+c1+".1/"+e1);
-                    if (fin>255){//si pasa de 255
-                        int sob = fin-255-1;
-                        result.setText(a1+"."+(b1++)+"."+sob+".254/"+e1);
-                        System.out.println(a1+"."+(b1++)+"."+sob+".254/"+e1);
-                    }
-                    else
-                        df1.setText(a1+"."+b1+"."+fin+".254/"+e1);
-                    System.out.println(a1+"."+b1+"."+fin+".254/"+e1);
-                }
-
-            }
-
-
-
-            else {//cuarto octeto direccion ip
-                int resta = 32 - e1;
-                int salto = (int) Math.pow(2, resta);
-                int fin = d1+salto-1;
-                mr1.setText(a1+"."+b1+"."+c1+".0/"+e1);
-                di1.setText(a1+"."+b1+"."+c1+".1/"+e1);
-                if (fin>255){//si pasa de 255
-                    int sob = fin-255-1;
-                    df1.setText(a1+"."+b1+"."+(c1+1)+"."+sob+"/"+e1);
-                }
-                else
-                    df1.setText(a1+"."+b1+"."+c1+"."+d1+"/"+e1);
-            }
-
-        }
-
-        else {
-            ver.setText("Division / incorrecta");
-        }
-
-
-    }
 
     @Override
     public void onClick(View view) {
-        //Verificar que no esten los campos vacios
+        sub();
+    }
+
+    public void sub(){
+        int m;
+        String c2;
         if (a.getText().toString().isEmpty() || b.getText().toString().isEmpty() || c.getText().toString().isEmpty() || d.getText().toString().isEmpty() || div.getText().toString().isEmpty()){
-            ver.setText("Llene los campos");
+
+            Toast.makeText(this,"De favor llena todos los campos",Toast.LENGTH_LONG).show();
         }
         else {// si no estan vacios
-            int a1 = Integer.parseInt(a.getText().toString());
-            int b1 = Integer.parseInt(b.getText().toString());
-            int c1 = Integer.parseInt(c.getText().toString());
-            int d1 = Integer.parseInt(d.getText().toString());
-            int e1 = Integer.parseInt(div.getText().toString());
-            if (a1>255 || b1>255 || c1> 255 || d1 > 254 ){// que no pasen de 255
+            ver.setText("  ");
+            int u = Integer.parseInt(a.getText().toString());
+            int d = Integer.parseInt(b.getText().toString());
+            int t = Integer.parseInt(c.getText().toString());
+            int c = Integer.parseInt(q.getText().toString());
+            int n = Integer.parseInt(div.getText().toString());
+            noRedes=(EditText) findViewById(R.id.noRedes);
+            int nu= Integer.parseInt(noRedes.getText().toString());
+            int a=0,f,g = 0,g2=0,n2=1,D=n;
+            int s=d;
+            int g1=d,g12=t,g13=c;
+            if (u > 255 || d > 255 || t > 255 || c > 254) {// que no pasen de 255
                 ver.setText("Verifique direccion ip");
+                Toast.makeText(this,"Verifica la direccion IP ingresada",Toast.LENGTH_LONG).show();
             }
-            else {//si la direccion es correcta verificar la division
-                if (e1<=30 ) {
-                    ver.setText("Direccion y division correcta");
-
-                    //probando
-                    if (e1 < 8) {
-                        int resta = 8 - e1;//saber la posicion de la ultima x en el octeto
-                        int salto = (int) Math.pow(2, resta);//proximo salto para referencia
-                        int fin = a1 + salto - 1;
-                        mr1.setText(a1 + ".0.0.0");
-                        di1.setText(a1 + ".0.0.1");
-                        if (fin > 255) {//si pasa de 255
-                            int sob = fin - 255 - 1;
-                            df1.setText((a1 + 1) + "." + sob + ".255.254/" + e1);
-                        } else
-                            df1.setText(fin + ".255.255.254/" + e1);
+            else{
+                System.out.println(u);
+                int f2=c;
+                f2++;
+                di1.setText(""+u+"."+d+"."+t+"."+f2+"/"+nu);
+                    if(n>=24){
+                        n=n-24;
+                        n2=4;
+                    } else if (n>=16){
+                        n=n-16;
+                        n2=3;
+                    }else if (n>=8){
+                        n=n-8;
+                        n2=2;
                     }
 
-                    else if (e1 > 8 && e1 <= 16) {//segundo octeto direccion ip
-                        int resta = 16 - e1;
-                        int salto = (int) Math.pow(2, resta);
-                        int fin = b1 + salto - 1;
-                        mr1.setText(a1 + "." + b1 + ".0.0/" + e1);
-                        di1.setText(a1 + "." + b1 + ".0.1/" + e1);
-                        if (fin > 255) {//si pasa de 255
-                            int sob = fin -255- 1;
-                            df1.setText((a1 + 1) + "." + sob + ".255.254/" + e1);
-                        }
-                        else
-                            df1.setText(a1 + "." + fin + ".255.254/" + e1);
-                        }
-                    else if (e1>16 && e1<=24 ){//tercer octeto direccion ip
-                        int resta = 24 - e1;
-                        int salto = (int) Math.pow(2, resta);
-                        int fin = c1+salto-1;
-                        mr1.setText(a1+"."+b1+"."+c1+".0/"+e1);
-                        di1.setText(a1+"."+b1+"."+c1+".1/"+e1);
-                        if (fin>255){//si pasa de 255
-                            int sob = fin-255-1;
-                            df1.setText(a1+"."+(b1+1)+"."+sob+".254/"+e1);
-                        }
-                        else
-                            df1.setText(a1+"."+b1+"."+fin+".254/"+e1);
-                        }
-
-
-
-                    else {//cuarto octeto direccion ip
-                        int resta = 32 - e1;
-                        int salto = (int) Math.pow(2, resta);
-                        int fin = d1+salto-1;
-                        mr1.setText(a1+"."+b1+"."+c1+".0/"+e1);
-                        di1.setText(a1+"."+b1+"."+c1+".1/"+e1);
-                        if (fin>255){//si pasa de 255
-                            int sob = fin-255-1;
-                            df1.setText(a1+"."+b1+"."+(c1+1)+"."+sob+"/"+e1);
-                        }
-                        else
-                            df1.setText(a1+"."+b1+"."+c1+"."+d1+"/"+e1);
-                        }
-
+                    if (n==1){
+                        g=128;
+                        g2=128;
+                    }else if(n==2){
+                        g=64;
+                        g2=192;
+                    }else if(n==3){
+                        g=32;
+                        g2=224;
+                    }else if(n==4){
+                        g=16;
+                        g2=240;
+                    }else if(n==5){
+                        g=8;
+                        g2=248;
+                    }else if(n==6){
+                        g=4;
+                        g2=252;
+                    }else if(n==7){
+                        g=2;
+                        g2=254;
+                    }else if(n==8){
+                        g=1;
+                        g2=255;
                     }
 
-                else {
-                    ver.setText("Division / incorrecta");
+                    if(n2==1){
+                        mr1.setText(g2+".0.0.0");
+                    }else if(n2==2){
+                        mr1.setText("255."+g2+".0.0");
+                    }else if(n2==3){
+                        mr1.setText("255.255."+g2+".0");
+                    }else if(n2==4){
+                        mr1.setText("255.255.255"+g2);
+                    }
+                if(n2==1){
+                 int aux=u+g-1;
+                    c--;
+                    c--;
+                    d--;
+                    t--;
+
+                    if(d<0){
+                        d=255;
+                    }
+                    if(t<0){
+                        t=255;
+                    }
+                    if(c<0){
+                        c=254;
+                    }
+                    df1.setText(aux+" "+d+" "+t+" "+c);
+                    System.out.println(aux+" "+d+" "+t+" "+c);
+                }else if(n2==2){
+                    int aux=d+g-1;
+                    c--;
+                    c--;
+                    t--;
+
+
+                    if(t<0){
+                        t=255;
+                    }
+                    if(c<0){
+                        c=254;
+                    }
+                    df1.setText(u+"."+aux+"."+t+"."+c);
+                }else if(n2==3){
+                    int aux=t+g-1;
+                    c--;
+                    c--;
+                    if(c<0){
+                        c=254;
+                    }
+                    df1.setText(u+"."+d+"."+aux+"."+c);
+
+                }else if(n2==4){
                 }
+
+                    int p=0,mo=2,mo2;
+                int i,e=0;
+                double x=0;
+                for(i=0;i<10;i++) {
+                    i--;
+                    x = Math.pow(2, p);
+                    if(x>=nu){
+                        i=100;
+                    }else{
+                        p++;
+                    }
+                }
+
+
+                n=D+p;
+                System.out.println("Numero de saltos final: "+n+" "+p);
+                if(n>=24){
+                    n=n-24;
+                    n2=4;
+                } else if (n>=16){
+                    n=n-16;
+                    n2=3;
+                }else if (n>=8){
+                    n=n-8;
+                    n2=2;
+                }
+
+                int k=n;
+                System.out.println("Num Sal: "+k);
+                if (k==1){
+                    g=128;
+                    g2=128;
+                }else if(k==2){
+                    g=64;
+                    g2=192;
+                }else if(k==3){
+                    g=32;
+                    g2=224;
+                }else if(k==4){
+                    g=16;
+                    g2=240;
+                }else if(k==5){
+                    g=8;
+                    g2=248;
+                }else if(k==6){
+                    g=4;
+                    g2=252;
+                }else if(k==7){
+                    g=2;
+                    g2=254;
+                }else if(k==8){
+                    g=1;
+                    g2=255;
+                }
+                System.out.println("s tiebe: "+s);
+                System.out.println("Numero de saltos: "+g);
+
+                    result2.setText("Subredes\n");
+                if(n2==0){
+                    mr1.setText(g2+".0.0.0");
+                }else if(n2==2){
+                    for(i=0;i<nu;i++){
+                        s=s+g;
+                        System.out.println(s);
+                        result2.setText(result2.getText()+"\n"+u+"."+(s-8)+"."+g12+"."+g13);
+                    }
+                }else if(n2==3){
+                    mr1.setText("255.255."+g2+".0");
+                    for(i=0;i<nu;i++){
+                        s=s+g;
+                        result2.setText(result2.getText()+"\n"+u+"."+g1+"."+(s-8)+"."+g12);
+                    }
+                }else if(n2==4){
+                    mr1.setText("255.255.255"+g2);for(i=0;i<nu;i++){
+                        s=s+g;
+                        result2.setText(result2.getText()+"\n"+u+"."+g1+"."+g12+"."+(s-8));
+                    }
+                }
+                System.out.println(p);
+
             }
-            noRedes();
+
+
         }
     }
+
 }
+
+
+
